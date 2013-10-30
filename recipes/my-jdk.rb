b55a6475e564818c7a07666868c8f8ce9f9f910a
cbbarclay <cbbarclay@hotmail.com>
2013-10-30 12:53:52 -0700

    Create my-jdk.rb

---
+++ recipes/my-jdk.rb
@@ -0,0 +1,17 @@
+source_url = "http://s3.amazonaws.com/your-bucket/your-jdk.zip"
+jdk_uri = ::URI.parse(source_url)
+jdk_filename = ::File.basename(jdk_uri.path)
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{jdk_filename}" do
+  source source_url
+  mode 00755
+  notifies :run, "execute[install-custom-java]", :immediately
+end
+
+execute "install-custom-java" do
+  cwd Chef::Config[:file_cache_path]
+  command "./#{jdk_filename} -f ./installer.properties -i silent"
+  creates "#{node['java']['java_home']}/jre/bin/java"
+end
+
+include_recipe "java::set_java_home"
